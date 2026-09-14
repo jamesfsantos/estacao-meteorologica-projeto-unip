958200af9008a7169c389ccc4fa8daa80b2b6892
James Santos <jamesf.santos@hotmail.com>
2026-09-14 17:19:45 -0300

    feature: Endpoint buscando dados no BROQUER MQTT

--- api/main.py
+++ api/main.py
@@ -1,38 +1,58 @@
+from contextlib import asynccontextmanager
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
+import paho.mqtt.client as mqtt
 
-app = FastAPI()
 
 class DadosTemperatura(BaseModel):
-    temperatura: str
-    umidade: str
-    pressao: str
-    temperatura_bmp: str
-    luminosidade: str
-    gas: str
-    gas_digital: str
+    temperatura: str = "0.0"
+    umidade: str = "0.0"
+    pressao: str = "0"
+    temperatura_bmp: str = "0.0"
+    luminosidade: str = "0"
+    gas: str = "0"
+    gas_digital: str = "0"
+
+dados_estacao = DadosTemperatura()
+
+# 2. Configuração dos Callbacks MQTT
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Conectado ao Broker MQTT! Código: {reason_code}")
     
-    
-mock_temperatura = {
-    "temperatura_bmp": "2.9", 
-    "gas_digital": "0", 
-    "pressao": "83747", 
-    "luminosidade": "1001", 
-    "gas": "3628", 
-    "temperatura": "54.3", 
-    "umidade": "72.5"
-}
+    # Aqui lê onde vai pegar as mensagens.
+    client.subscribe("projeto-unip/estacao-meteorologica/temperatura")
 
-#model_valiade --> Converte o json para a classe que chamo
-dados = DadosTemperatura.model_validate(mock_temperatura)
+def on_message(client, userdata, msg):
+    global dados_estacao #Modica 
+    try:
+        payload = msg.payload.decode("utf-8")
+        dados_json = json.loads(payload)
+        
+        
+        dados_estacao = DadosTemperatura.model_validate(dados_json)
+        print(f"Dados atualizados via MQTT: {dados_estacao}")
+    except Exception as e:
+        print(f"Erro ao processar mensagem MQTT: {e}")
 
 
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    mqttc.connect("broker.hivemq.com", 1883, 60)
+    mqttc.loop_start()
+    yield # Esse é o responsavel por detectar quando encerra a aplicação: CTRL + C..
+    mqttc.loop_stop()
+    mqttc.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
+
+# 6. Endpoints
 @app.get("/")
 def read_root():
-    return {"dados": dados}
-
-
-
-
-
-
+    return dados_estacao
