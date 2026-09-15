57729e11b5a1e58a6c8f353749af7b91e99692fc
James Santos <jamesf.santos@hotmail.com>
2026-09-15 10:08:42 -0300

    Substituindo projeto

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+export function App() {
+  return (
+    <main>
+      <h1>Estação Meteorológica</h1>
+      <p>Projeto pronto para desenvolvimento.</p>
+    </main>
+  );
+}
+
+export default App;
